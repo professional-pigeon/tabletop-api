93ae01e3b28f621c05414796415086d9c4e2ed9e
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-12 16:41:35 -0700

    migrate user, create user controller

--- db/migrate/20230412233402_create_users.rb
+++ db/migrate/20230412233402_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :user_name
-      t.string :email
-      t.string :password
+      t.string :user_name, null: false
+      t.string :email, null: false, index: { unique: true, name: 'unique_emails' }
+      t.string :password_digest, null: false
 
       t.timestamps
     end
