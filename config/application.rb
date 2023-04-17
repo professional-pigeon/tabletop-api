dc7447d0fc1cc9af2bc7622c30d2ed03ac0d18dc
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-17 13:49:49 -0700

    update cookie name

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module TabletopApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_namespace_key'
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_api_tabletop_management_session'
   end
 end
